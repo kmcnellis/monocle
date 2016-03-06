b24d182366bdd7f74d1dfaca9ea6ca3b1245fb5a
Kiana McNellis <kiana@kmcnellis.com>
2016-03-06 10:33:05 -0500

    Added pins

--- edison/app.js
+++ edison/app.js
@@ -12,12 +12,14 @@ Cylon.robot({
     rgb_led: { driver: 'rgb-led', redPin: 3, greenPin: 5, bluePin: 6 },
 
     //inputs:
-    button: { driver: 'button', pin: 2 }
+    button: { driver: 'button', pin: 2 },
+    ambient: { driver: 'analog-sensor', pin: 0, lowerLimit: 100, upperLimit: 900 },
+    color_detector: { driver: 'analog-sensor', pin: 1, lowerLimit: 0, upperLimit: 1024 },
 
   },
   work: function(my) {
     my.button.on('push', function() {
-      my.led.toggle();
+      my.laser.toggle();
     });
   },
 
