03fb0255711ce2accbfa8bdcb7d41cacc309f31a
root <root@localhost.localdomain>
2016-03-06 14:57:01 +0000

    adding tests

--- edison/app.js
+++ edison/app.js
@@ -7,12 +7,34 @@ Cylon.robot({
     edison: { adaptor: 'intel-iot' }
   },
   devices: {
-    led: { driver: 'led', pin: 13 },
+    //outputs:
+    laser: { driver: 'led', pin: 13 },
+    rgb_led: { driver: 'rgb-led', redPin: 3, greenPin: 5, bluePin: 6 },
+
+    //inputs:
     button: { driver: 'button', pin: 2 }
+
   },
   work: function(my) {
     my.button.on('push', function() {
       my.led.toggle();
     });
+  },
+
+  detect_color: function() {
+
+  },
+
+  output_sound: function(color) {
+
+  },
+
+  warn_distance: function() {
+
+  },
+
+  warn_tilt: function() {
+
   }
+
 }).start();
