fa04268b55fc021c62902c3b3afc3a4a5140e4ac
Kiana McNellis <kiana@kmcnellis.com>
2016-03-06 11:54:26 -0500

    Updates for edison and logo

--- edison/app.js
+++ edison/app.js
@@ -1,6 +1,18 @@
 "use strict";
 
 var Cylon = require('cylon')
+var tilted = false;
+var Player = require('player');
+var talking = false;
+var red = new Player('./mp3/red.mp3');
+var green = new Player('./mp3/green.mp3');
+var blue = new Player('./mp3/blue.mp3');
+var leds = {red:2,blue:3,green:4};
+var color = "";
+var whiteArray = {red:0,blue:0,green:0};
+var blackArray = {red:0,blue:0,green:0};
+var colorArray = {red:0,blue:0,green:0};
+
 
 Cylon.robot({
   connections: {
@@ -13,6 +25,7 @@ Cylon.robot({
 
     //inputs:
     button: { driver: 'button', pin: 2 },
+    tilt: { driver: 'button', pin: 4 },
     ambient: { driver: 'analog-sensor', pin: 0, lowerLimit: 100, upperLimit: 900 },
     color_detector: { driver: 'analog-sensor', pin: 1, lowerLimit: 0, upperLimit: 1024 },
 
@@ -28,15 +41,100 @@ Cylon.robot({
   },
 
   output_sound: function(color) {
+    console.log(color + " detected");
+    if (!talking){
+        if (color === "red"){
 
-  },
+            red.play(function(err, player){
+                if (err){
+                    console.log("Error playing red",err);
+                }
+                console.log('red playend!');
+                talking = false;
+            });
 
-  warn_distance: function() {
-
+        }
+        if (color === "green"){
+            green.play(function(err, player){
+                if (err){
+                    console.log("Error playing green",err);
+                }
+                console.log('green playend!');
+                talking = false;
+            });
+        }
+        if (color === "blue"){
+            blue.play(function(err, player){
+                if (err){
+                    console.log("Error playing blue",err);
+                }
+                console.log('blue playend!');
+                talking = false;
+            });
+        }
+    }
   },
 
   warn_tilt: function() {
+    my.tilt.on('push', function() {
+      console.log("Tilt Activated");
+      tilted = true;
+    });
+    my.tilt.on('release', function() {
+      console.log("Tilt Deactivated");
+      tilted = false;
+    });
 
+},
+  setup: function(my) {
+    var setRed = function(){
+        my.rgb_led.setRGB("FF0000")
+    };
+    var setBlue = function(){
+        my.rgb_led.setRGB("0000FF")
+    }
+    var setGreen = function(){
+        my.rgb_led.setRGB("00FF00")
+    }
+
+    var calibrate(){ 
+            setRed();
+            after((0.001).seconds(), function() {
+                getReading(5, function(avgRead){
+                    whiteArray.red = avgRead;
+                });
+            }
+
+            digitalWrite(ledArray[i], LOW);
+            delay(100);
+        }
+
+
+        }
+    var getReading = function(num, cb){
+        if (num ===0){
+            cb();
+        }
+
+    }
+    every((1).seconds(), function() {
+      console.log("Hello, human!");
+    });
+    after((0.5).seconds(), function() {
+        setRed();
+        after((0.001).seconds(), function() {
+         getReading(5);
+         whiteArray[i] = avgRead;
+         digitalWrite(ledArray[i], LOW);
+         delay(100);
+      }
+
+    });
+
+
+    });
   }
+});
+
 
 }).start();
--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'itosApp',
+        module: 'monocleApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
